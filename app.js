ae57e727ff1aa12ae223ab4a4fc516f4353fbf9a
lukechidubem <lukechidubem@gmail.com>
2022-11-16 09:03:57 +0100

    worked on order and product route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
-
+import orderRouter from "./routes/orderRoutes.js";
 import dotenv from "dotenv";
 
 const app = express();
@@ -23,9 +23,24 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.get("/api/keys/paypal", (req, res) => {
+//   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+// });
+
+// app.get("/api/keys/google", (req, res) => {
+//   res.send({ key: process.env.GOOGLE_API_KEY || "" });
+// });
+
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/upload", uploadRouter);
+app.use("/api/orders", orderRouter);
+
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+// );
 
 const port = process.env.PORT || 6000;
 app.listen(port, () => {
